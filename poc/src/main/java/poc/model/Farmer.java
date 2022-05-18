d8a7c6a34cbb4f037a5c623eac086c75b765f2c3
joshi <joshi@DESKTOP-DQOF977>
2022-05-18 13:53:00 +0530

    new enhancement

--- poc/src/main/java/poc/model/Farmer.java
+++ poc/src/main/java/poc/model/Farmer.java
@@ -1,6 +1,5 @@
 package poc.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -10,7 +9,6 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
